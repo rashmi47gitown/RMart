a341b79eba833d1e1ccf1ef17c14e67aa8d234e7
Rsmi <143258969+rashmi47gitown@users.noreply.github.com>
2024-06-26 15:22:53 +0530

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -9,32 +9,20 @@ import {
 } from "../controllers/categoryController.js";
 
 const router = expres.Router();
-
-//routes
-
-//create category
 router.post(
   "/create-category",
   requireSignIn,
   isAdmin,
   createCategoryController
 );
-
-//update category
 router.put(
   "/update-category/:id",
   requireSignIn,
   isAdmin,
   updateCategoryController
 );
-
-//get all categories
 router.get("/get-category", getCategoryController);
-
-//single category
 router.get("/single-category/:slug", singleCategoryController);
-
-//delete category
 router.delete(
   "/delete-category/:id",
   requireSignIn,
