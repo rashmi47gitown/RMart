ac32779365a9231e8aaa6351b422c5553cfeae2b
Rsmi <143258969+rashmi47gitown@users.noreply.github.com>
2024-06-26 15:24:45 +0530

    Update productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -22,8 +22,6 @@ import formidable from "express-formidable";
 import braintree from "braintree";
 
 const router = express.Router();
-
-//CRAETE PRODUCT
 router.post(
   "/create-product",
   requireSignIn,
@@ -32,22 +30,12 @@ router.post(
   createProductController
 );
 
-//GET PRODUCT
 router.get("/get-product", getProductController);
-
-//GET SINGLE PRODUCT
 router.get("/single-product/:slug", singleProductController);
-
-//get single pro using slug
 router.get("/get-product/:slug", getSingleProductController);
-
-//GET PHOTO
 router.get("/product-photo/:pid", productPhotoController);
-
-//photo by pid
+//Photo by pid
 // router.get("/product-photo/:id", getProductPhotoController);
-
-//UPDATE
 router.put(
   "/update-product/:pid",
   requireSignIn,
@@ -55,34 +43,24 @@ router.put(
   formidable(),
   updateProductController
 );
-
-//DELETE PRODUCT
 router.delete(
   "/delete-product/:pid",
   requireSignIn,
   isAdmin,
   deleteProductController
 );
-
-//FILTER
 router.post("/product-filters", productFilterController);
 
-//PAGINATION
 router.get("/product-count", productCountController);
 
-//LIST
 router.get("/product-list/:page", productListController);
 
-//serach product
 router.get("/search/:keyword", searchProductController);
 
-//similar product
 router.get("/related-product/:pid/:cid", relatedProductController);
 
-//category wise product
 router.get("/product-catgeory/:slug", productCategoryController);
 
-//payment
 router.get("/braintree/token", braintreeTokenController);
 
 router.post("/braintree/payment", requireSignIn, braintreePaymentController);
