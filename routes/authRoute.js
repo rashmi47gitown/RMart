ed86a48c7e3f72a3c46df3a713ebc244a124f996
Rsmi <143258969+rashmi47gitown@users.noreply.github.com>
2024-06-26 15:22:10 +0530

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -14,12 +14,12 @@ const router = express.Router();
 router.post("/register", registerController);
 router.post("/login", loginController);
 router.get("/test", requireSignIn, isAdmin, testController);
-//Protect user route
+//Protected user route
 router.get("/user-auth", requireSignIn, (req, res) => {
   res.status(200).send({ ok: true });
 });
 
-//protected user route
+//protected admin route
 router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
   res.status(200).send({ ok: true });
 });
